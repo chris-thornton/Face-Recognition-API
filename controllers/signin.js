17cf8d31e96e4ea11087b586a81ef22343bab984
Chris Thornton <ctthornt@gmail.com>
2019-04-10 18:17:19 -0700

    added alert for failed login attempts

--- controllers/signin.js
+++ controllers/signin.js
@@ -15,7 +15,8 @@ const handleSignin = (req, res, db, bcrypt) => {
 					})
 					.catch(err => res.status(400).json('unable to get user'))
 			} else {
-				res.status(400).json('invalid credentials')
+				res.status(400).json('invalid credentials');
+				alert("invalid credentials");
 			}
 		})
 		.catch(err => res.status(400).json('invalid credentials'))
